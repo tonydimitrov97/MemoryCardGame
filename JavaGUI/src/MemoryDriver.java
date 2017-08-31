74eec9dadbb5deff1cc733f18a56d9a108e2d75a
tonydimitrov97 <30800247+tonydimitrov97@users.noreply.github.com>
2017-08-31 15:16:18 -0500

    Update MemoryDriver.java

--- JavaGUI/src/MemoryDriver.java
+++ JavaGUI/src/MemoryDriver.java
@@ -1,10 +1,6 @@
 /**
  * memoryDriver.java
  * Main Driver which creates a new memory game
- * Date: 5-11-17
- * Java 1125
- * Spring 2017
- * Homework Assignment 5
  * @author Antonio Dimitrov
  */
 public class MemoryDriver {
